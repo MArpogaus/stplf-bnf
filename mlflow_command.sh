12f8215f2c64c16e9c12b86a1b734cd9cb68e418
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2022-03-02 15:42:41 +0000

    comments unneeded webdav code

--- mlflow_command.sh
+++ mlflow_command.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -eux
 
-DVC_REMOTE_DEFAULT=$(dvc remote default)
+# uncomment for use with webdav remote
+#DVC_REMOTE_DEFAULT=$(dvc remote default)
 
-[ ! -e .dvc/config.local ] && {
-    dvc remote modify --local $DVC_REMOTE_DEFAULT user $DVC_REMOTE_USER
-    dvc remote modify --local $DVC_REMOTE_DEFAULT password $DVC_REMOTE_PASS
-}
+#[ ! -e .dvc/config.local ] && {
+#    dvc remote modify --local $DVC_REMOTE_DEFAULT user $DVC_REMOTE_USER
+#    dvc remote modify --local $DVC_REMOTE_DEFAULT password $DVC_REMOTE_PASS
+#}
 
-# dvc pull --run-cache
-# dvc repro $@
 dvc exp run $@
